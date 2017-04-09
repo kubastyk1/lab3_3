ff7e776ca624caf15e0737c8b1239583fde23ab1
Jakub Stawiszyński <kubastyk@wp.pl>
2017-04-10 01:01:03 +0200

    added DefaultTimeSrc class

---
+++ src/main/java/edu/iis/mto/time/DefaultTimeSrc.java
@@ -0,0 +1,10 @@
+package edu.iis.mto.time;
+
+public final class DefaultTimeSrc implements TimeSource {
+
+ 	@Override
+ 	public long currentTimeMillis() {
+ 		return System.currentTimeMillis();
+ 	}
+
+ }
